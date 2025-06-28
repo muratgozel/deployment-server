78b1c7a212219763f18a06088bab1c11fe81b664
Murat Gözel <murat@gozel.com.tr>
2025-06-28 12:14:18 +0300

    fix: celery beat scheduler db dir

--- src/deployment_server/beat.py
+++ src/deployment_server/beat.py
@@ -6,5 +6,9 @@ from deployment_server.modules import env
 if __name__ == "__main__":
     init()
 
+    schedule_db_dir = (
+        "/var/lib/prod-deployment-server-25-http" if env.is_prod() else "./"
+    )
+
     worker = create_worker()
-    worker.start(["beat", "--loglevel=info"])
+    worker.start(["beat", "--loglevel=info", "--schedule", schedule_db_dir])
